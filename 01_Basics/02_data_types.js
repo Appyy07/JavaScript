d21b01c2eea9037a346e6d2557daa3c0e4d452ef
Appyy07 <154001996+Appyy07@users.noreply.github.com>
2024-07-24 08:30:33 +0000

    data types

---
+++ 01_Basics/02_data_types.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all JS code as newer version
+// alert (3+3)  //we cannot use this syntax as this is NodeJS and not a browser
+
+console.log(3+3)
+console.log("Appy")  //code readability should be good
